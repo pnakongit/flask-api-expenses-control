3f37fb37808788d62e682a0963d0f42d8778762d
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-08-27 13:32:35 +0300

    Add expense_create view to app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify, Response
+from urllib import request
+
+from flask import Flask, jsonify, Response, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
@@ -27,7 +29,7 @@ def index() -> (Response, int):
     return jsonify({"message": "Hello World!"}), 200
 
 
-@app.route("/expenses", methods=["GET", ])
+@app.route("/expenses/", methods=["GET", ])
 def expenses_list() -> (Response, int):
     expenses = Expense.query.all()
     return jsonify(
@@ -35,10 +37,18 @@ def expenses_list() -> (Response, int):
     ), 200
 
 
-@app.route("/expenses/<int:expense_id>", methods=["GET", ])
+@app.route("/expenses/<int:expense_id>/", methods=["GET", ])
 def expense_detail(expense_id: int) -> (Response, int):
     expense = db.get_or_404(Expense, expense_id)
     return jsonify({"id": expense.id, "title": expense.title}), 200
+
+
+@app.route("/expenses/", methods=["POST", ])
+def expense_create() -> (Response, int):
+    new_expense = Expense(title=request.json["title"])
+    db.session.add(new_expense)
+    db.session.commit()
+    return jsonify({"id": new_expense.id, "title": new_expense.title}), 201
 
 
 @app.errorhandler(404)
