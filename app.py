265507f13637d7174c3d79f6d71e0587a08d3bc7
Pavlo Nakonechnyi <p.nakonechniy@gmail.com>
2024-08-27 12:32:21 +0300

    Add db with model Expense

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, jsonify, Response
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 
 app = Flask(__name__)
+
+app.config.from_mapping(SQLALCHEMY_DATABASE_URI="sqlite:///project.db")
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+class Expense(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
 
 
 @app.route("/", methods=["GET", ])
@@ -9,4 +27,6 @@ def index() -> (Response, int):
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
